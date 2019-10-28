7d67eaf1d751f97a1fef5f03ef4b70855b57fbaf
trevorrwarduk <trevorrwarduk@gmail.com>
2019-10-28 13:50:10 +0000

    Removed old code

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -24,31 +24,4 @@ winston.loggers.add("customLogger", {
 
 const logger = winston.loggers.get("customLogger");
 
-// const logger = winston.createLogger({
-//     level: "info",
-//     format: winston.format.json(),
-//     defaultMeta: { service: "user-service" },
-//     transports: [
-//         new winston.transports.File({ filename: "log/error.log", level: "error" }),
-//         new winston.transports.File({ filename: "log/info.log", level: "info" })
-//     ]
-// });
-
-// const logger1 = winston.createLogger({
-//     level: "info",
-//     format: winston.format.json(),
-//     defaultMeta: { service: "user-service" },
-//     transports: [new winston.transports.File({ filename: "log/info.log", level: "info" })]
-// });
-
-/* eslint-disable */
-// eslint is reporting - 'process' is not defined  no-undef
-// if (process.env.NODE_ENV === "local") {
-//     /* eslint-enable */
-//     logger.add(
-//         new winston.transports.Console({
-//             format: winston.format.simple()
-//         })
-//     );
-// }
 module.exports = logger;
