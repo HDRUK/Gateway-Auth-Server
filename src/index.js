cff8c41471d79b0298a9355bf881ed7b74315516
trevorrwarduk <trevorrwarduk@gmail.com>
2019-10-30 09:42:07 +0000

    Fixed eslint disabled issues

--- src/index.js
+++ src/index.js
@@ -5,11 +5,8 @@ require("dotenv").config();
 
 const logger = require("./utils/logger");
 
-/* eslint-disable */
-//  error  'process' is not defined  no-undef
 const port = process.env.PORT;
 const mainApp = process.env.APPLICATION_PATH;
-/* eslint-enable */
 
 app.use(express.static(mainApp));
 
