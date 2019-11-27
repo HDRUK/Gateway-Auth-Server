e64289cddef5def7273703249ec5da6b3c35fcfe
Kathryn Ellington <Kathryn.Ellington@ibm.com>
2019-11-27 17:49:28 +0000

    Update openid-connect log in

--- src/index.js
+++ src/index.js
@@ -6,70 +6,64 @@ const passport = require("passport");
 
 const logger = require("./utils/logger");
 
-// const { Issuer, generators } = require("openid-client");
-
 const port = process.env.PORT;
 const mainApp = process.env.APPLICATION_PATH;
 
 app.use(express.static(mainApp));
 
-// app.get("/login", function(req, res) {
-app.get("/login", passport.authenticate("saml"), function(req, res) {
-    // If this function gets called, authentication was successful.
-    // `req.user` contains the authenticated user.
-    res.redirect("/users/" + req.user.username);
-    // (async () => {
-    // const issuer = await Issuer.discover("https://connect.openathens.net");
+const session = require("express-session");
 
-    // const openAthensIssuer = new Issuer({
-    //     issuer: process.env.AuthProviderURI,
-    //     authorization_endpoint: process.env.AuthLoginEndPoint,
-    //     token_endpoint: process.env.AuthTokenEndPoint,
-    //     userinfo_endpoint: process.env.AuthUserInfoEndPoint,
-    //     jwks_uri: process.env.AuthJWKSURI
-    // });
+const OidcStrategy = require("passport-openidconnect").Strategy;
+const baseAuthUrl = "https://connect.openathens.net";
 
-    // console.log(openAthensIssuer);
+app.use(
+    session({
+        secret: "MyVoiceIsMyPassportVerifyMe",
+        resave: false,
+        saveUninitialized: true
+    })
+);
 
-    // return openAthensIssuer;
-    // Issuer.discover(process.env.AuthProviderURI)
-    //     .then(function(athensIssuer) {
-    //         console.log("Login Result Path 123 ", athensIssuer.issuer);
-    //         const client = new athensIssuer.Client({
-    //             client_id: process.env.AuthClientID,
-    //             client_secret: process.env.AuthClientsecret,
-    //             redirect_uris: [process.env.AuthRedirectURL]
-    //             // response_types: [process.env.AuthIdentityKey]
-    //             // id_token_signed_response_alg (default "RS256")
-    //             // token_endpoint_auth_method (default "client_secret_basic")
-    //         });
-    //         const code_verifier = generators.codeVerifier();
+app.use(passport.initialize());
+app.use(passport.session());
 
-    //         const code_challenge = generators.codeChallenge(code_verifier);
+passport.use(
+    "oidc",
+    new OidcStrategy(
+        {
+            issuer: baseAuthUrl,
+            authorizationURL: baseAuthUrl + "/oidc/auth",
+            tokenURL: baseAuthUrl + "/oidc/token",
+            userInfoURL: baseAuthUrl + "/oidc/userinfo",
+            clientID: "hdruk.ac.uk.oidc-app-v1.5614d0c5-9fbf-46a0-8311-baf34df79bdb",
+            clientSecret: "IsRerNSo3JdPSWgo2X3amjcfy",
+            callbackURL: "http://localhost:5003/redirect"
+        },
+        (issuer, sub, profile, accessToken, refreshToken, done) => {
+            return done(null, profile);
+        }
+    )
+);
 
-    //         client.authorizationUrl({
-    //             scope: "openid email profile",
-    //             resource: process.env.AuthLoginEndPoint,
-    //             code_challenge,
-    //             code_challenge_method: "S256"
-    //         });
-    //         // const params = client.callbackParams(req);
-    //         // client
-    //         //     .callback("http://localhost:5003/callback", params, { code_verifier }) // => Promise
-    //         //     .then(function(tokenSet) {
-    //         //         console.log("received and validated tokens %j", tokenSet);
-    //         //         console.log("validated ID Token claims %j", tokenSet.claims());
-    //         //     });
-    //         // console.log("CLIENT ", client, " .... ");
-    //     })
-    //     .catch(function(err) {
-    //         console.log("FAIL ", err);
-    //     });
-    // const Client = issuer.Client;
-    // console.log("Login Result Path 123");
-    // })();
-    console.log("Login Result Path");
-    return res;
+passport.serializeUser((user, next) => {
+    next(null, user);
+});
+
+passport.deserializeUser((user, next) => {
+    console.log(user._json.eduPersonTargetedID);
+    next(null, user);
+});
+
+app.use("/login", passport.authenticate("oidc"));
+
+app.use("/redirect", passport.authenticate("oidc", { failureRedirect: "/error" }), (req, res) => {
+    res.redirect("/");
+});
+
+app.get("/logout", (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.redirect("/");
 });
 
 app.listen(port, () =>
